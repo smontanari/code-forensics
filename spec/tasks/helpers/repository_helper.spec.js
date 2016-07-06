c223bd606a3c9c78ade26400cbdf4d9cff36c556
Silvio Montanari <montanari.silvio@gmail.com>
2016-07-06 16:19:03 +1000

    sum of coupling diagram

--- spec/tasks/helpers/repository_helper.spec.js
+++ spec/tasks/helpers/repository_helper.spec.js
@@ -4,49 +4,35 @@ var repositoryHelper = require_src('tasks/helpers/repository_helper');
 
 describe('repositoryHelper', function() {
   beforeEach(function() {
-    this.repository = {
-      root: '/repo-root',
-      isValidPath: function(p) { return /^test\/valid-path\/.*/.test(p); }
-    };
+    this.repository = jasmine.createSpyObj('isValidPath', ['isValidPath']);
     this.inputStream = new stream.PassThrough({objectMode: true});
   });
 
   describe('.validPathFilter()', function() {
-    it('includes only paths that are valid', function(done) {
-      var output = [];
-      this.inputStream.pipe(repositoryHelper.validPathFilter(this.repository))
-      .on('data', output.push.bind(output))
-      .on('end', function() {
-        expect(output).toEqual([
-          { id: 'file1', path: 'test/valid-path/file1' },
-          { id: 'file3', path: 'test/valid-path/file3' }
-        ]);
-        done();
+    describe('when the path is relative', function() {
+      it('includes only relative paths that are valid', function(done) {
+        var self = this;
+        this.repository.isValidPath.and.callFake(function(p) { return /^test\/valid-path\/.*/.test(p); });
+
+        var output = [];
+        this.inputStream.pipe(repositoryHelper.validPathFilter(this.repository))
+        .on('data', output.push.bind(output))
+        .on('end', function() {
+          expect(output).toEqual([
+            { id: 'file1', path: 'test/valid-path/file1' },
+            { id: 'file3', path: 'test/valid-path/file3' }
+          ]);
+          expect(self.repository.isValidPath).toHaveBeenCalledWith('test/valid-path/file1', true);
+          expect(self.repository.isValidPath).toHaveBeenCalledWith('test/invalid-path/file2', true);
+          expect(self.repository.isValidPath).toHaveBeenCalledWith('test/valid-path/file3', true);
+          done();
+        });
+
+        this.inputStream.write({ id: 'file1', path: 'test/valid-path/file1' });
+        this.inputStream.write({ id: 'file2', path: 'test/invalid-path/file2' });
+        this.inputStream.write({ id: 'file3', path: 'test/valid-path/file3' });
+        this.inputStream.end();
       });
-
-      this.inputStream.write({ id: 'file1', path: 'test/valid-path/file1' });
-      this.inputStream.write({ id: 'file2', path: 'test/invalid-path/file2' });
-      this.inputStream.write({ id: 'file3', path: 'test/valid-path/file3' });
-      this.inputStream.end();
-    });
-  });
-
-  describe('.absolutePathMapper()', function() {
-    it('converts the repo relative paths to absolute paths', function(done) {
-      var output = [];
-      this.inputStream.pipe(repositoryHelper.absolutePathMapper(this.repository))
-      .on('data', output.push.bind(output))
-      .on('end', function() {
-        expect(output).toEqual([
-          { id: 'file1', path: '/repo-root/test/file1' },
-          { id: 'file2', path: '/repo-root/test/file2' }
-        ]);
-        done();
-      });
-
-      this.inputStream.write({ id: 'file1', path: 'test/file1' });
-      this.inputStream.write({ id: 'file2', path: 'test/file2' });
-      this.inputStream.end();
     });
   });
 });
