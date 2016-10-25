94641e540b86bbda9e9b22fde08eed4647ca928a
Silvio Montanari <montanari.silvio@gmail.com>
2016-10-25 17:20:41 +0100

    refactored revision stream helper

--- spec/tasks/helpers/revision_helper.spec.js
+++ spec/tasks/helpers/revision_helper.spec.js
@@ -2,26 +2,30 @@ var stream = require('stream'),
     map    = require("through2-map"),
     moment = require('moment');
 
-var RevisionStreamHelper = require_src('tasks/helpers/revision_stream_helper'),
-    vcsSupport           = require_src('vcs_support'),
-    pp                   = require_src('parallel_processing'),
-    utils                = require_src('utils');
+var RevisionHelper = require_src('tasks/helpers/revision_helper'),
+    vcsSupport     = require_src('vcs_support'),
+    pp             = require_src('parallel_processing'),
+    utils          = require_src('utils');
 
-describe('RevisionStreamHelper', function() {
+describe('RevisionHelper', function() {
   beforeEach(function() {
     this.mockVcs = jasmine.createSpyObj('vcs adapter', ['revisions', 'showRevisionStream']);
     spyOn(vcsSupport, 'adapter').and.returnValue(this.mockVcs);
 
-    this.subject = new RevisionStreamHelper({
-      repository: { rootPath: 'repo_root' }
-    }, 'jobRunner');
+    this.subject = new RevisionHelper({
+      repository: { rootPath: 'repo_root' },
+      parameters: { targetFile: '/test/file' },
+      dateRange: 'date-range'
+    });
   });
 
-  describe('.revisionAnalysisStream()', function() {
-    var analyserFn = function() {
-      return map.obj(function(obj) {
-        return { result: obj.analysis + '-result' };
-      });
+  describe('.revisionComplexityStream()', function() {
+    var analyser = {
+      sourceAnalysisStream: function() {
+        return map.obj(function(obj) {
+          return { result: obj.analysis + '-result' };
+        });
+      }
     };
 
     beforeEach(function() {
@@ -39,7 +43,7 @@ describe('RevisionStreamHelper', function() {
       spyOn(utils.arrays, 'arrayToFnFactory').and.returnValue('revisions');
       this.mockStreamCollector.mergeAll.and.returnValue('final stream');
 
-      expect(this.subject.revisionAnalysisStream('/test/file', 'date-range')).toEqual('final stream');
+      expect(this.subject.revisionComplexityStream(analyser)).toEqual('final stream');
 
       expect(this.mockVcs.revisions).toHaveBeenCalledWith('/test/file', 'date-range');
       expect(utils.arrays.arrayToFnFactory).toHaveBeenCalledWith(['revision1', 'revision2'], jasmine.any(Function));
@@ -55,7 +59,7 @@ describe('RevisionStreamHelper', function() {
       var revisionStream = new stream.PassThrough({ objectMode: true });
       this.mockVcs.showRevisionStream.and.returnValue(revisionStream);
 
-      this.subject.revisionAnalysisStream('/test/file', 'date-range', analyserFn);
+      this.subject.revisionComplexityStream(analyser);
       streamAnalysisFn({ revisionId: '123', date: '2014-01-31' })
         .on('data', function(obj) {
           expect(obj.revision).toEqual('123');
