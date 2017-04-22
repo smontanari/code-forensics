e5e116c41ec8576f103647703505a42de1a521be
Silvio Montanari <smontanari@mail.com>
2017-04-23 09:24:59 +1000

    refactored some xml functions into utils

---
+++ spec/utils/xml_utils.spec.js
@@ -0,0 +1,25 @@
+var XmlUtils = require_src('utils/xml_utils');
+
+describe('XmlUtils', function() {
+  describe('nodeWithName()', function() {
+    it('returns a resolver for the xml tag with the given name', function() {
+      var fn = XmlUtils.nodeWithName('test_name');
+
+      expect(fn({ name: 'test_name' })).toEqual(true);
+      expect(fn({ name: 'another_name' })).toEqual(false);
+    });
+  });
+
+  describe('nodeText()', function() {
+    it('returns text content of the given xml node', function() {
+      var node = {
+        children: [
+          { type: 'attribute', value: 'test_attribute' },
+          { type: 'text', value: 'test_text' }
+        ]
+      }
+
+      expect(XmlUtils.nodeText(node)).toEqual('test_text');
+    });
+  });
+});
--- spec/vcs_support/git/gitlog_stream_transformer.spec.js
+++ spec/vcs_support/git/gitlog_stream_transformer.spec.js
@@ -1,9 +1,9 @@
 var _      = require('lodash'),
     stream = require('stream');
 
-var LogNormaliser = require_src('vcs_support/git/gitlog_stream_transformer.js');
+var LogStreamTransformer = require_src('vcs_support/git/gitlog_stream_transformer.js');
 
-describe('GitLogNormaliser', function() {
+describe('GitLogStreamTransformer', function() {
   describe('Author name normalisation', function() {
     beforeEach(function() {
       var stubDeveloperInfo = {
@@ -15,7 +15,7 @@ describe('GitLogNormaliser', function() {
       var stubRepository = {
         isValidPath: function() { return true; }
       };
-      this.subject = new LogNormaliser(stubRepository, stubDeveloperInfo);
+      this.subject = new LogStreamTransformer(stubRepository, stubDeveloperInfo);
     });
 
     it('streams log lines with author name changed according to the developer info', function(done) {
@@ -45,19 +45,19 @@ describe('GitLogNormaliser', function() {
         });
 
       var logLines = [
-        '--98b656f--2016-10-31--Developer 1',
-        '--f7633f6--2016-10-31--Developer 3',
-        '10  0 test/file1.yml.erb',
-        '',
-        '--6ff89bc--2016-10-31--Developer_2',
-        '1 1 test/file2.rb',
-        '',
-        '--02790fd--2016-10-31--Developer.3',
-        '--5fbfb14--2016-10-28--Alias developer 2',
-        '0 1 test/file3.rb',
-        '0 20  test/file4.html.erb',
-        '6 8 test/file5.js'
-      ].join("\n");
+        "--98b656f--2016-10-31--Developer 1\n",
+        "--f7633f6--2016-10-31--Developer 3\n",
+        "10  0 test/file1.yml.erb\n",
+        '\n',
+        "--6ff89bc--2016-10-31--Developer_2\n",
+        "1 1 test/file2.rb\n",
+        '\n',
+        "--02790fd--2016-10-31--Developer.3\n",
+        "--5fbfb14--2016-10-28--Alias developer 2\n",
+        "0 1 test/file3.rb\n",
+        "0 20  test/file4.html.erb\n",
+        "6 8 test/file5.js"
+      ];
 
       _.each(logLines, logStream.push.bind(logStream));
       logStream.end();
@@ -74,7 +74,7 @@ describe('GitLogNormaliser', function() {
           return path !== 'test/invalid_file.rb';
         }
       };
-      this.subject = new LogNormaliser(stubRepository, stubDeveloperInfo);
+      this.subject = new LogStreamTransformer(stubRepository, stubDeveloperInfo);
     });
 
     it('streams log lines filtering out commits with invalid file paths', function(done) {
@@ -103,20 +103,20 @@ describe('GitLogNormaliser', function() {
         });
 
       var logLines = [
-        '--98b656f--2016-10-31--Developer 1',
-        "1\t17\ttest/some/file123.rb",
-        "42\t0\ttest/another/file456.rb",
-        "10\t0\ttest/file1.yml.erb",
-        '',
-        '--6ff89bc--2016-10-31--Developer_2',
-        "1\t1\ttest/invalid_file.rb",
-        '',
-        '--02790fd--2016-10-31--Developer.3',
-        '--5fbfb14--2016-10-28--Developer_2',
-        "0\t1\ttest/file3.rb",
-        "-\t-\ttest/invalid_file.rb",
+        "--98b656f--2016-10-31--Developer 1\n",
+        "1\t17\ttest/some/file123.rb\n",
+        "42\t0\ttest/another/file456.rb\n",
+        "10\t0\ttest/file1.yml.erb\n",
+        "\n",
+        "--6ff89bc--2016-10-31--Developer_2\n",
+        "1\t1\ttest/invalid_file.rb\n",
+        "\n",
+        "--02790fd--2016-10-31--Developer.3\n",
+        "--5fbfb14--2016-10-28--Developer_2\n",
+        "0\t1\ttest/file3.rb\n",
+        "-\t-\ttest/invalid_file.rb\n",
         "6\t8\ttest/file5.js"
-      ].join("\n");
+      ];
 
       _.each(logLines, logStream.push.bind(logStream));
       logStream.end();
