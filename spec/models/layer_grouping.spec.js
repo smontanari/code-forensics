fb36f461cfe91ada134d5500909e312f7a6d5320
Silvio Montanari <smontanari@mail.com>
2018-08-29 08:20:05 +1000

    enable generating individual layer grouping files

--- spec/models/layer_grouping.spec.js
+++ spec/models/layer_grouping.spec.js
@@ -7,46 +7,52 @@ describe('LayerGrouping', function() {
     expect(new LayerGrouping([]).isEmpty()).toBe(true);
   });
 
-  it('returns the a simple text representating of the layer path mappings', function() {
-    var layers = new LayerGrouping([
-      { name: "Layer1", paths: ['test/path1', 'test/path2'] },
-      { name: "Layer2", paths: ['test/path3'] }
-    ]);
+  describe('all layers', function() {
+    it('returns the path mappings representation of all the layers', function() {
+      var layers = new LayerGrouping([
+        { name: 'Layer 1', paths: ['test/path1', 'test/path2', /test\/path3\/((?!.*--abc\.)).*\/files/] },
+        { name: 'a_Layer 2', paths: ['test/path4', /test\/path3\/.*--abc\/files/] },
+        { name: 'another layer-3', paths: ['test/path5'] }
+      ]);
 
-    expect(layers.toString()).toEqual([
-      'test/path1 => Layer1',
-      'test/path2 => Layer1',
-      'test/path3 => Layer2'
-    ].join("\n"));
+      expect(layers.toString()).toEqual([
+        'test/path1 => Layer 1',
+        'test/path2 => Layer 1',
+        '^test\\/path3\\/((?!.*--abc\\.)).*\\/files$ => Layer 1',
+        'test/path4 => a_Layer 2',
+        '^test\\/path3\\/.*--abc\\/files$ => a_Layer 2',
+        'test/path5 => another layer-3'
+      ].join("\n"));
+    });
   });
 
-  it('returns the regexp representation of the layer path mappings', function() {
-    var layers = new LayerGrouping([
-      { name: "Layer1", paths: [/^test\/path1/, /test\/path2$/] },
-      { name: "Layer2", paths: [/test\/path3\/((?!.*--abc\.)).*\/files/] }
-    ]);
+  describe('individual layers', function() {
+    it('returns the text path mappings representation for each individual layer', function() {
+      var layers = new LayerGrouping([
+        { name: 'Layer 1', paths: ['test/path1', 'test/path2', /test\/path3\/((?!.*--abc\.)).*\/files/] },
+        { name: 'a_Layer 2', paths: ['test/path4', /test\/path3\/.*--abc\/files/] },
+        { name: 'another layer-3', paths: ['test/path5'] }
+      ]);
 
-    expect(layers.toString()).toEqual([
-      '^test\\/path1$ => Layer1',
-      '^test\\/path2$ => Layer1',
-      '^test\\/path3\\/((?!.*--abc\\.)).*\\/files$ => Layer2'
-    ].join("\n"));
-  });
+      var allLayers = layers.map(function(layer) { return layer; });
 
-  it('returns the mixed text representation of the layer path mappings', function() {
-    var layers = new LayerGrouping([
-      { name: "Layer1", paths: ['test/path1', 'test/path2', /test\/path3\/((?!.*--abc\.)).*\/files/] },
-      { name: "Layer2", paths: ['test/path4', /test\/path3\/.*--abc\/files/] },
-      { name: "Layer3", paths: ['test/path5'] }
-    ]);
+      expect(allLayers[0].name).toEqual('layer-1');
+      expect(allLayers[0].toString()).toEqual([
+        'test/path1 => Layer 1',
+        'test/path2 => Layer 1',
+        '^test\\/path3\\/((?!.*--abc\\.)).*\\/files$ => Layer 1'
+      ].join("\n"));
 
-    expect(layers.toString()).toEqual([
-      'test/path1 => Layer1',
-      'test/path2 => Layer1',
-      '^test\\/path3\\/((?!.*--abc\\.)).*\\/files$ => Layer1',
-      'test/path4 => Layer2',
-      '^test\\/path3\\/.*--abc\\/files$ => Layer2',
-      'test/path5 => Layer3'
-    ].join("\n"));
+      expect(allLayers[1].name).toEqual('a-layer-2');
+      expect(allLayers[1].toString()).toEqual([
+        'test/path4 => a_Layer 2',
+        '^test\\/path3\\/.*--abc\\/files$ => a_Layer 2'
+      ].join("\n"));
+
+      expect(allLayers[2].name).toEqual('another-layer-3');
+      expect(allLayers[2].toString()).toEqual([
+        'test/path5 => another layer-3'
+      ].join("\n"));
+    });
   });
 });
