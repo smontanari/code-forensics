35e6f7298270ce98636c0ae99587485c6d52e2ff
Silvio Montanari <smontanari@mail.com>
2018-12-31 21:07:03 +1100

    removed dependency on fancy-log

---
+++ spec/log/logger.spec.js
@@ -0,0 +1,53 @@
+/*global require_src*/
+var _     = require('lodash'),
+    chalk = require('chalk');
+
+var logger = require_src('log/logger');
+
+describe('Logger', function() {
+
+  var assertStdout = function(msg) {
+    expect(process.stdout.write.calls.count()).toEqual(2);
+    expect(process.stdout.write.calls.argsFor(0)).toEqual(['[21:14:57] ']);
+    expect(process.stdout.write.calls.argsFor(1)).toEqual([msg + '\n', jasmine.any(Function)]);
+  };
+
+  beforeEach(function() {
+    jasmine.clock().install();
+    jasmine.clock().mockDate(new Date('2015-10-22T10:14:57.000Z'));
+    _.each(['log', 'info', 'error', 'warn'], function(m) {
+      logger[m].and.callThrough();
+    });
+    spyOn(process.stdout, 'write');
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+  });
+
+  it('prefixes any log with a timestamp', function() {
+    logger.log('test message');
+
+    assertStdout('test message');
+  });
+
+  it('writes a debug message with a detail', function() {
+    logger.debug('test message: ', 'test detail');
+    assertStdout(chalk.green('test message: ') + 'test detail');
+  });
+
+  it('writes an info message with a detail', function() {
+    logger.info('test message: ', 'test detail');
+    assertStdout(chalk.yellow('test message: ') + 'test detail');
+  });
+
+  it('writes a warning message', function() {
+    logger.warn('test message');
+    assertStdout(chalk.bgWhite.magenta('test message'));
+  });
+
+  it('writes an error message', function() {
+    logger.error('test message');
+    assertStdout(chalk.bgWhite.red('test message'));
+  });
+});
