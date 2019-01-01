4715f893a180963cd35b10ccd05317634886c6f2
Silvio Montanari <smontanari@mail.com>
2019-01-02 08:35:06 +1100

    updated copyright header

--- scripts/webserver.js
+++ scripts/webserver.js
@@ -2,7 +2,7 @@
 
 /*
  * code-forensics
- * Copyright (C) 2016-2018 Silvio Montanari
+ * Copyright (C) 2016-2019 Silvio Montanari
  * Distributed under the GNU General Public License v3.0
  * see http://www.gnu.org/licenses/gpl.html
  */
